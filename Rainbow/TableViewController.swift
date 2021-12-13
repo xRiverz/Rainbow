150128b1be3d56ccedc5fb47a706e1791691b59a
Anhar AG <85056292+xRiverz@users.noreply.github.com>
2021-12-13 18:04:44 +0300

    Create TableViewController.swift

---
+++ Rainbow/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  UITableViewCellAnimation-Article
+//
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+	@IBAction func onRefresh(_ sender: UIBarButtonItem) {
+		// Refresh table view 
+	}
+
+	// MARK: - Table view data source and delegate methods
+
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+	}
+}
